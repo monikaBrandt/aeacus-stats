775fc74ed9f51a0cfa6c68b215e462593063a55e
monikaBrandt <monika.brandt@medsci.uu.se>
2017-05-21 17:50:25 +0200

    Fixed update of list with scores

--- sampleFastq.py
+++ sampleFastq.py
@@ -4,44 +4,52 @@ import numpy
 import pandas as pd
 
 def parseFastq(filen,nr):
+    sampleDict={}
     si=1
     for record in SeqIO.parse(filen, "fastq"):#This loop is per read, not per row.
         if si % 10 == 0:
-            print(si)
-            getQscore(record.seq,record.letter_annotations["phred_quality"])
+            #print(si)
+            scoreList=getQscore(record.seq,record.letter_annotations["phred_quality"])
+            sampleDict[str(si)] = scoreList
         si+=1
+    return sampleDict
 
 def getQscore(seq,qscore):
-    print seq,qscore   
-    for i in range(0, len(seq)):
-        print(qScorePerBasePosA[i+1])
-        if seq[i] == 'A':
-            print(i)
-            print(i+1)
-            print(qscore[i])
-            qScorePerBasePosA[i+1].append(qscore[i])
-            print(qScorePerBasePosA[i+1])
+    print seq,qscore
+    readnScores=list()   
+    for i in range(1, len(seq)+1):
+        if seq[i-1] == 'A':
+            #qScorePerBasePosA[i].append(qscore[i-1])
+            readnScores.append(qscore[i-1])
+            print(qscore[i-1])
         else:
-            qScorePerBasePosA[i+1]#.append(None)
+            #qScorePerBasePosA[i].append(None)
+            readnScores.append(None)
+            print None
+    return readnScores
     
 
 def calculateQStats(nframe):
     nmean = nframe.apply(numpy.mean,axis=0)
     nstd = nframe.apply(numpy.std,axis=0)
-    statsPerBasePosN = dict.fromkeys(range(1,readlen+1),list())
-    return(statsPerBasePosN)
+    #statsPerBasePosN = dict.fromkeys(range(1,readlen+1),list())
+    #return(statsPerBasePosN)
+    print nmean
+    print nstd
     
 filename=sys.argv[1]#fastq filename
-print(filename)
 nrow=sys.argv[2]#number of rows in fastq file
-print(nrow)
 readlen=151
 qScorePerBasePosA = dict.fromkeys(range(1,readlen+1),list())
 print(qScorePerBasePosA)
-parseFastq(filename,nrow)
+smpD = parseFastq(filename,nrow)
+#seqstr='NCCACATTTTCAAGAT'
+#scorenr=[2, 12, 32, 32, 37, 37, 41, 37, 41, 41, 41, 41, 37, 37, 41, 41]
 #print(qScorePerBasePosA)
-#aframe = pd.DataFrame.from_dict(qScorePerBasePosA,orient='columns')
-#print(aframe)
-#meanstda=calculateQStats(aframe)
-#print(meanstda)
+aframe = pd.DataFrame(smpD)
+print(aframe)
+trframe=pd.DataFrame.transpose(aframe)
+print(trframe)
+calculateQStats(trframe)
+
 
